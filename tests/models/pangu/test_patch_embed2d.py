069fdef98dc462e7024dabbee6000fd12e91d053
Li zhuoqun <m15004059308@163.com>
2023-11-28 15:52:13 +0800

    add: add pangu embed function & test

--- tests/models/pangu/test_patch_embed2d.py
+++ tests/models/pangu/test_patch_embed2d.py
@@ -3,10 +3,10 @@ import unittest
 import torch
 from torch import nn
 
-from WeatherLearn.models.layers.patch_embed import PatchEmbed2D, PatchEmbed3D
+from WeatherLearn.models.pangu.utils.patch_embed import PatchEmbed2D, PatchEmbed3D
 
 
-class PatchEmbed2DTest(unittest.TestCase):
+class PatchEmbedTest(unittest.TestCase):
     def test_patch_embed2d_with_pad(self):
         img = torch.rand(2, 4, 7, 7)
         pe_2d = PatchEmbed2D((7, 7), (2, 2), 4, 8)
@@ -69,3 +69,25 @@ class PatchEmbed2DTest(unittest.TestCase):
         pe_3d_3 = PatchEmbed3D((3, 7, 8), (2, 3, 3), 4, 8)
         out_3 = pe_3d_3(img_3)
         self.assertEqual(out_3.shape, (2, 8, 2, 3, 3))
+
+    def test_patch_embed2d_pangu(self):
+        surface = torch.randn(1, 4 + 3, 721, 1440)
+        patchembed2d = PatchEmbed2D(
+            img_size=(721, 1440),
+            patch_size=(4, 4),
+            in_chans=4 + 3,
+            embed_dim=1
+        )
+        surface_embed = patchembed2d(surface)
+        self.assertEqual(surface_embed.shape, (1, 1, 181, 360))
+
+    def test_patch_embed3d_pangu(self):
+        upper_air = torch.randn(1, 5, 13, 721, 1440)
+        patchembed3d = PatchEmbed3D(
+            img_size=(13, 721, 1440),
+            patch_size=(2, 4, 4),
+            in_chans=5,
+            embed_dim=1
+        )
+        upper_air_embed = patchembed3d(upper_air)
+        self.assertEqual(upper_air_embed.shape, (1, 1, 7, 181, 360))
